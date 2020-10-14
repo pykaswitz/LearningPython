52c4e0a4364d99cb463c774bf5ba56027c0a4468
Brian <bwjl03@gmail.com>
2020-10-14 01:04:41 -0600

    Start of mid-term project file.  Adding project comments
    Signed-off-by: BrianW <bwjl03@gmail.com>

---
+++ Python 1/mid_term_project.py
@@ -0,0 +1,63 @@
+# Brian Wolfe                       Created: 10/14/2020
+
+'''
+
+**** CONSIDER USING ASSIGNMENT 5A CODE AS BASE FOR THIS PROJECT ****
+
+Code a menu-driven application that does the following:
+
+The user can enter data for a product name, product code, and unit price.
+
+An example might be 'Braeburn Apples', 'BAP'. 1.99.  After entering the data, it is appended (note I said 'appended')
+as a single line of text to a text file.  Each line of text in the file should represent a single product.
+
+There is an option to display the contents of the text file.
+
+The program runs until the user tells it to stop.
+
+Here's a possible run of your program.
+
+Inventory program
+1) Enter a product
+2) Display all products
+3) Quit
+Select a menu option: 1
+
+Enter a product
+Name: Braeburn Apples
+Code: BAP
+Price: 1.99
+
+Inventory program
+1) Enter a product
+2) Display all products
+3) Quit
+Select a menu option: 1
+
+Enter a product
+Name: Joliet Pears
+Code: JPR
+Price: 3.09
+
+Inventory program
+1) Enter a product
+2) Display all products
+3) Quit
+Select a menu option: 2
+
+Products on file...
+Braeburn Apples, BAP, 1.99
+Joliet Pears, JPR, 3.09
+
+Inventory program
+1) Enter a product
+2) Display all products
+3) Quit
+Select a menu option: 3
+
+The program has ended.
+
+If your text file doesn't exist and you attempt to display it, your code will fail.  At this time, don't worry about
+that.  However, if you want to code for that potential problem, feel free.  You never know when that might be useful.
+
+'''
