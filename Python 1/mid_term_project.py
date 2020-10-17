b1258fcf51b2c58e6ce2741f8d61789f4223e636
Brian <bwjl03@gmail.com>
2020-10-17 02:02:25 -0600

    Updates: read file converts to list-of-list, formated printed output of file data
    Signed-off-by: Brian <bwjl03@gmail.com>

--- Python 1/mid_term_project.py
+++ Python 1/mid_term_project.py
@@ -21,16 +21,27 @@ def display_menu():
 def append_to_file(file_path, content_list):
     # Convert list to string, APPEND string to file_path
     content_list[2] = str(format(content_list[2], '.2f'))  # Format float 2 decimal then to string
-    content_str = ", ".join(content_list)  # Ready list for write to file adding commas
+    content_str = ",".join(content_list)  # Ready list for write to file adding commas
     with open(file_path, "a") as fin:
         fin.write(content_str)
         fin.write("\n")
 
 
+# Chose to reconstitute string into list of lists so that if needed price can easily be converted back to float.
+def convert_to_listoflists(tangled_strings):
+    lol = []  # ready a list of list variable
+    for line in tangled_strings:
+        line = line.strip("\n")
+        li = list(line.split(","))
+        lol.append(li)
+    return lol
+
+
 def read_from_file(file_path):
     # Read the file contents and return
     with open(file_path, "r") as fout:
-        return fout.readlines()
+        file_dump = fout.readlines()
+        return convert_to_listoflists(file_dump)
 
 
 def main():
@@ -60,11 +71,15 @@ def main():
         elif menu_option == "2":
             products = read_from_file(FILENAME)
 
-            print("----------------------------------------")
+            # Using string formats https://docs.python.org/2.7/library/string.html#format-specification-mini-language
+            # Detect character length and fills that in within the allotted spacing per cell
+            # < ^ > for alignment, number for cell spacing and * for 'unpacking' values
             print("Products on file...")
             print("----------------------------------------")
-            for item in range(len(products)):
-                print(products[item], end="")
+            print("{: ^20}{: <9}{: <10}".format('Name', 'Code', 'Price'))
+            print("----------------------------------------")
+            for product in products:
+                print("{: <20}{: <9}${: <10}".format(*product))
             print("-------------------EoF-------------------")
             print()
 
@@ -77,4 +92,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
