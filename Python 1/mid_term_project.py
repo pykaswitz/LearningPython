6e3bed76aa793b1fa2791550e06011a1955cee66
Brian <bwjl03@gmail.com>
2020-10-17 02:41:20 -0600

    Added exception handling, FINAL UPDATE - Submitting project.
    Signed-off-by: Brian <bwjl03@gmail.com>

--- Python 1/mid_term_project.py
+++ Python 1/mid_term_project.py
@@ -12,9 +12,13 @@ def display_header():
 
 
 def display_menu():
+    print("Please select an option from the menu.")
+    print()
+    print("------------------MENU------------------")
     print("1. Enter a product")
     print("2. Display all products")
     print("3. Exit")
+    print("----------------------------------------")
     print()
 
 
@@ -50,44 +54,85 @@ def main():
     display_header()
 
     while menu_looping:
-        product = []
+        try:
+            product = []
 
-        display_menu()
-        menu_option = input("Please choose a number option from the menu: ")
-        print()
-
-        if menu_option == "1":
-            print("---------------")
-            print("Enter a Product")
-            print("---------------")
-            product.append(input("Name: ").title())  # Title case entry
-            product.append(input("Code: ").upper())  # Upper case entry
-            product.append(float(input("Price: $")))  # Convert to float
-
-            append_to_file(FILENAME, product)
-
-            print(f"\nProduct added to {FILENAME}.\n")
-
-        elif menu_option == "2":
-            products = read_from_file(FILENAME)
-
-            # Using string formats https://docs.python.org/2.7/library/string.html#format-specification-mini-language
-            # Detect character length and fills that in within the allotted spacing per cell
-            # < ^ > for alignment, number for cell spacing and * for 'unpacking' values
-            print("Products on file...")
-            print("----------------------------------------")
-            print("{: ^20}{: <9}{: <10}".format('Name', 'Code', 'Price'))
-            print("----------------------------------------")
-            for product in products:
-                print("{: <20}{: <9}${: <10}".format(*product))
-            print("-------------------EoF-------------------")
+            display_menu()
+            menu_option = input("Please choose a number option from the menu: ")
             print()
 
-        elif menu_option == "3":
-            menu_looping = False
+            if menu_option == "1":
+                print("---------------")
+                print("Enter a Product")
+                print("---------------")
 
-        else:
-            print("Invalid menu selection.  Please try again.\n")
+                testing = True
+                while testing:
+                    product.append(input("Name: ").title())  # Title case entry
+                    if len(product[0]) <= 0:
+                        print("\nERROR: This is not a valid name.\n")
+                        product.pop()  # Resets the list to remove the invalid entry
+                    else:
+                        testing = False
+
+                testing = True  # Resets for testing the next entry.
+                while testing:
+                    product.append(input("Code: ").upper())  # Upper case entry
+                    if len(product[1]) != 3:
+                        print("\nERROR: This is not a valid code of 3 characters.\n")
+                        product.pop()  # Resets the list to remove the invalid entry
+                    else:
+                        testing = False
+
+                testing = True  # Resets for testing the next entry.
+                while testing:
+                    try:
+                        product.append(float(input("Price: $")))  # Convert to float
+                        if product[2] <= 0.0:
+                            print("\nERROR: This is not a valid price.\n")
+                            product.pop()  # Resets the list to remove the invalid entry
+                        else:
+                            testing = False
+                    except ValueError:
+                        # Catches exception if you try to enter nothing
+                        print("\nERROR: You must enter a positive amount.\n")
+                    except Exception as ex:
+                        print(f"\nUNKNOWN ERROR: A exception has occurred: {ex}\n")
+                        raise
+
+                append_to_file(FILENAME, product)
+
+                print(f"\nProduct added to {FILENAME}.\n")
+
+            elif menu_option == "2":
+                products = read_from_file(FILENAME)
+
+                # Using string formats https://docs.python.org/2.7/library/string.html#format-specification-mini-language
+                # Detect character length and fills that in within the allotted spacing per cell
+                # < ^ > for alignment, number for cell spacing and * for 'unpacking' values
+                print("Products on file...")
+                print("----------------------------------------")
+                print("{: ^20}{: <9}{: <10}".format('Name', 'Code', 'Price'))
+                print("----------------------------------------")
+                for product in products:
+                    print("{: <20}{: <9}${: <10}".format(*product))
+                print("-------------------EoF-------------------")
+                print()
+
+            elif menu_option == "3":
+                print("Thank you. Good bye!")
+                menu_looping = False
+
+            else:
+                print("ERROR: Invalid menu selection.  Please try again.\n")
+        except FileNotFoundError:
+            print()
+            print(f"ERROR: The file {FILENAME} could not be found.")
+            print("\t   Please choose Option 1 to create the file.")
+            print()
+        except Exception as ex:
+            print(f"\nUNKNOWN ERROR: A exception has occurred: {ex}\n")
+            raise
 
 
 if __name__ == '__main__':
